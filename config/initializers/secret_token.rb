aa8072e22d7057e4444d95017f1163436f971d97
chiperific <chiperific@gmail.com>
2014-06-10 17:15:43 -0400

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WmreccTimesheet::Application.config.secret_key_base = '2f0906a718bb1f3d4bdaea624b50f708beda819b1b71a46d1a439925a605cff04cfca2a0423baf5163c423419ef4db250bcb1e0defadff26d83935c9bb51728c'
