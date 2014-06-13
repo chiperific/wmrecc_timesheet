82f4c5f9a12fc556e811bdb65bd2acbd023179be
chiperific <chiperific@gmail.com>
2014-06-13 14:22:12 -0400

    finish user form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,33 @@ class UsersController < ApplicationController
 
   def new
     @title = "Add User"
+    @user = User.new
   end
 
   def edit
     @title = "Edit User"
     @user = User.find(params[:id])
-  end
+
+    
+    @super_array = Array.new
+    @super_array << ["(no supervisor)", nil]
+    User.all.sort_by { |u| u.fname }.sort_by { |u| u.lname}.each do |u|
+      user_array = Array.new
+      user_array << (u.fname + " " + u.lname)
+      user_array << u.id
+      @super_array << user_array
+    end
+
+    @dept_array = Array.new
+    @dept_array << ["(no department)", nil]
+    Department.all.sort_by { |d| d.name }.each do |d|
+      ddl_array = Array.new
+      ddl_array << d.name
+      ddl_array << d.id
+      @dept_array << ddl_array
+    end
+
+  end #edit
 
   def update
   end
