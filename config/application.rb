7e3a101491c664c46c9b2d71ebe5e5244e183349
chiperific <chiperific@gmail.com>
2014-09-16 21:50:15 -0400

    fix static assets in production.rb

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ Bundler.require(:default, Rails.env)
 module WmreccTimesheet
   class Application < Rails::Application
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.bmp)
-    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
