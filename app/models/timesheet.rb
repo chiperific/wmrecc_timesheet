af411fa7c7322653551646c0540561d882cecba2
chiperific <chiperific@gmail.com>
2015-01-12 08:59:52 -0500

    allow week_num to reach 53

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -6,7 +6,7 @@ class Timesheet < ActiveRecord::Base
   accepts_nested_attributes_for :timesheet_hours, :timesheet_categories
 
   validates :week_num, :year, presence: true
-  validates :week_num, numericality: { greater_than: 0, less_than_or_equal_to: 52 }
+  validates :week_num, numericality: { greater_than: 0, less_than_or_equal_to: 53 }
   validates :year, numericality: { greater_than_or_equal_to: 1900, less_than_or_equal_to: 2999 }
 
   def week_num_to_date
