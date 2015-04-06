6e8f40f4a1627d0e1ebb7e79f1100f28e3a82cb4
chiperific <chiperific@gmail.com>
2015-04-06 13:15:17 -0400

    fix timesheet_visual hidden box, reset table IDs following migration

---
+++ db/migrate/20150406165901_reset_ids.rb
@@ -0,0 +1,8 @@
+class ResetIds < ActiveRecord::Migration
+  def change
+    # Reset all the primary ID keys based upon seeded data.
+    ActiveRecord::Base.connection.reset_pk_sequence!('timesheets')
+    ActiveRecord::Base.connection.reset_pk_sequence!('timesheet_hours')
+    ActiveRecord::Base.connection.reset_pk_sequence!('timesheet_categories')
+  end
+end
